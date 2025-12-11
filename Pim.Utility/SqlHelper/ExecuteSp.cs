9a679604ad84aa45a6c25d92bf81b649d3550537
Priyan Vishwakarma <priyan.vishwakarma@nimapinfotech.com>
2025-12-11 17:56:48 +0530

    updated executeSp helper

--- Pim.Utility/SqlHelper/ExecuteSp.cs
+++ Pim.Utility/SqlHelper/ExecuteSp.cs
@@ -1,7 +1,8 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
 using Pim.Data;
 using System.Data;
-using System.Data.Common;
 
 namespace Pim.Utility.SqlHelper
 {
@@ -14,63 +15,121 @@ namespace Pim.Utility.SqlHelper
             _dbContext = dbContext;
         }
 
-        public async Task<List<TResult>> ExecuteStoredProcedureListAsync<TResult>(
+        public async Task<List<T>> ExecuteStoredProcedureListAsync<T>(
             string storedProcedure,
-            params DbParameter[] parameters)
-            where TResult : class, new()
+            params SqlParameter[] sqlParams)
         {
-            var results = new List<TResult>();
-
-            await using var connection = _dbContext.Database.GetDbConnection();
-            await using var command = connection.CreateCommand();
-
-            command.CommandText = storedProcedure;
-            command.CommandType = CommandType.StoredProcedure;
-
-            foreach (var p in parameters)
-                command.Parameters.Add(p);
+            var connection = _dbContext.Database.GetDbConnection();
 
             if (connection.State != ConnectionState.Open)
                 await connection.OpenAsync();
 
-            await using var reader = await command.ExecuteReaderAsync();
+            var dapperParams = ConvertToDynamicParameters(sqlParams);
 
-            var properties = typeof(TResult).GetProperties();
+            var rows = await connection.QueryAsync<T>(
+                storedProcedure,
+                dapperParams,
+                commandType: CommandType.StoredProcedure
+            );
 
-            while (await reader.ReadAsync())
+            return rows.AsList();
+        }
+
+        private DynamicParameters ConvertToDynamicParameters(SqlParameter[] sqlParams)
+        {
+            var dp = new DynamicParameters();
+
+            foreach (var p in sqlParams)
             {
-                var item = new TResult();
-
-                foreach (var prop in properties)
-                {
-                    if (!reader.HasColumn(prop.Name))
-                        continue;
-
-                    var value = reader[prop.Name];
-                    prop.SetValue(item, value == DBNull.Value ? null : value);
-                }
-
-                results.Add(item);
+                dp.Add(
+                    name: p.ParameterName,
+                    value: p.Value == DBNull.Value ? null : p.Value,
+                    dbType: (DbType?)p.SqlDbType,
+                    direction: p.Direction
+                );
             }
 
-            await reader.CloseAsync();
-
-            await command.DisposeAsync();
-
-            return results;
-        }
-    }
-
-    public static class DbDataReaderExtensions
-    {
-        public static bool HasColumn(this DbDataReader reader, string name)
-        {
-            for (int i = 0; i < reader.FieldCount; i++)
-                if (reader.GetName(i)
-                    .Equals(name, StringComparison.OrdinalIgnoreCase))
-                    return true;
-
-            return false;
+            return dp;
         }
     }
 }
+
+
+
+//using Microsoft.EntityFrameworkCore;
+//using Pim.Data;
+//using System.Data;
+//using System.Data.Common;
+
+//namespace Pim.Utility.SqlHelper
+//{
+//    public class ExecuteSp
+//    {
+//        private readonly ApplicationDbContext _dbContext;
+
+//        public ExecuteSp(ApplicationDbContext dbContext)
+//        {
+//            _dbContext = dbContext;
+//        }
+
+//        public async Task<List<TResult>> ExecuteStoredProcedureListAsync<TResult>(
+//            string storedProcedure,
+//            params DbParameter[] parameters)
+//            where TResult : class, new()
+//        {
+//            var results = new List<TResult>();
+
+//            await using var connection = _dbContext.Database.GetDbConnection();
+//            await using var command = connection.CreateCommand();
+
+//            command.CommandText = storedProcedure;
+//            command.CommandType = CommandType.StoredProcedure;
+
+//            foreach (var p in parameters)
+//                command.Parameters.Add(p);
+
+//            if (connection.State != ConnectionState.Open)
+//                await connection.OpenAsync();
+
+//            await using var reader = await command.ExecuteReaderAsync();
+
+//            var properties = typeof(TResult).GetProperties();
+
+//            while (await reader.ReadAsync())
+//            {
+//                var item = new TResult();
+
+//                foreach (var prop in properties)
+//                {
+//                    if (!reader.HasColumn(prop.Name))
+//                        continue;
+
+//                    var value = reader[prop.Name];
+//                    prop.SetValue(item, value == DBNull.Value ? null : value);
+//                }
+
+//                results.Add(item);
+//            }
+
+//            await reader.CloseAsync();
+
+//            await command.DisposeAsync();
+
+//            return results;
+//        }
+//    }
+
+//    public static class DbDataReaderExtensions
+//    {
+//        public static bool HasColumn(this DbDataReader reader, string name)
+//        {
+//            for (int i = 0; i < reader.FieldCount; i++)
+//                if (reader.GetName(i)
+//                    .Equals(name, StringComparison.OrdinalIgnoreCase))
+//                    return true;
+
+//            return false;
+//        }
+//    }
+//}
+
